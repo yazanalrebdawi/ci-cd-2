ceac275d1861fbb2e3e59b8101c3d58476585c4c
yazanalrebdawi <rebdawi2002@gmail.com>
2025-12-06 15:22:18 +0300

    edit comment

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,7 @@ class MyApp extends StatelessWidget {
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
